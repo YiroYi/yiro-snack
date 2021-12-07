ba604dfca07cb7cf3381afb7a46f5bf19cce6426
YiroYi <yiroyi@live.com>
2021-12-06 22:51:43 -0600

    Setting up the project

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Focus } from './src/features/focus/Focus';
+
+export default function App() {
+  const [focusSubject, setFocusSubject] = useState(null);
+  return (
+    <View style={styles.container}>
+      {focusSubject ? <Text> Here build a Timer</Text> : <Focus addSubject={setFocusSubject}/>}
+      <Text>{focusSubject}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#252250'
+  },
+});
